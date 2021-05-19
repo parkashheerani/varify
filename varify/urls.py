88bd581d35e17cfe5567fede68ee4adb6374cf0f
Parkash Heerani <parkashheerani99@gmail.com>
2021-05-20 01:45:47 +0500

    Update urls.py

--- varify/urls.py
+++ varify/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('', include('fakenews.urls')),
     path('admin/', admin.site.urls),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
