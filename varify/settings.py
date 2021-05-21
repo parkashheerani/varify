ddc6acf0987284818d00ee935076f3089bfc1e52
Parkash Heerani <parkashheerani99@gmail.com>
2021-05-21 20:36:52 +0500

    Update settings.py

--- varify/settings.py
+++ varify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k9wo_z63dkg+=zysqx(wmbi8r&9*+2fa3_@r0ozj5nqyz2l#3&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'urdufakenews.herokuapp.com']
 
